b6c4e0142acb478feb172415e8f43b8d808d7fba
Marc E. Colosimo <enzo69mc@gmail.com>
2024-10-24 07:47:38 -0400

    fixing index_of usage

--- src/main.v
+++ src/main.v
@@ -46,35 +46,38 @@ pub mut:
 // starting search from `from_index` position.
 @[direct_array_access]
 fn index_of(s string, p string, from_index ?int) ?int {
-        mut strt := 0
-		if from_index != none {
-			strt = from_index
-			if strt < 0 {
-                strt = 0
-        	}
+	// based v's index_of and on https://docs.oracle.com/javase/6/docs/api/java/lang/String.html#indexOf(java.lang.String,%20int)
+	// and https://docs.python.org/3/library/stdtypes.html#str.find
+	// see issue #804
+	mut strt := 0
+	if from_index != none {
+		strt = from_index
+		if strt < 0 {
+			strt = 0
 		}
-		if p.len > s.len {
-                return none
-        }
-        if strt >= s.len {
-                return none
-        }
-        mut i := strt
-        for i < s.len {
-                mut j := 0
-                mut ii := i
-                for j < p.len &&
-					ii < s.len &&
-					unsafe { s.str[ii] == p.str[j] } {
-                        j++
-                        ii++
-                }
-                if j == p.len {
-                        return i
-                }
-                i++
-        }
-        return none
+	}
+	if p.len > s.len {
+			return none
+	}
+	if strt >= s.len {
+			return none
+	}
+	mut i := strt
+	for i < s.len {
+			mut j := 0
+			mut ii := i
+			for j < p.len &&
+				ii < s.len &&
+				unsafe { s.str[ii] == p.str[j] } {
+					j++
+					ii++
+			}
+			if j == p.len {
+					return i
+			}
+			i++
+	}
+	return none
 }
 
 @[inline]
@@ -103,8 +106,7 @@ fn check_password(config SitConfig) !bool {
 		}
 
 		// Are there more asterices?
-		// Bad naming of function
-		// https://docs.python.org/3/library/stdtypes.html#str.find
+		// Bad naming of function, IMHO.
 		mut index := passwd.len
 		if wi < passwd.len {
 			index = index_of('passwd', '*', wi) or {
