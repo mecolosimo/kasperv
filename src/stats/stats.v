b9d3eff318da4260d46b8b26aa2f38f4063d9129
Marc E. Colosimo <enzo69mc@gmail.com>
2025-02-17 17:19:28 -0500

    small changes to stats

--- src/stats/stats.v
+++ src/stats/stats.v
@@ -1,6 +1,7 @@
 // Copyright (c) 2025 Marc E. Colosimo. All rights reserved.
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
+module stats
 
 import math
 
